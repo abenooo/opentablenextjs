14df2f2122a70df6a76b65317c534a7fdacdb3a5
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-07-04 14:23:43 +0300

    Add link

--- componet/NavBar.tsx
+++ componet/NavBar.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 function NavBar() {
   return (
     <nav className="bg-white p-2 flex justify-between">
-      <a href="" className="font-bold text-gray-700 text-2xl"> OpenTable </a>
+        <Link href="/" className="font-bold text-gray-700 text-2xl">
+          {" "} OpenTable{" "}
+        </Link>
       <div>
         <div className="flex">
           <button
