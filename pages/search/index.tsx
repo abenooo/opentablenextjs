d1c3530c44e8d4168eb3870c553973da3afe89a4
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-07-05 11:40:39 +0300

    Use DRY for footer and navbar

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,15 +1,10 @@
 import React from "react"
-import Link from "next/link"
 import Header from "./componet/Header"
-import NavBar from "@/componet/NavBar"
 import SearchSideBar from "./componet/SearchSideBar"
 import RestaurantCards from "./componet/RestaurantCards"
-import Footer from "@/componet/Footer"
 function Search() {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <NavBar />
+    <>
         <Header />
         <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
           <SearchSideBar />
@@ -17,9 +12,7 @@ function Search() {
             <RestaurantCards />
           </div>
         </div>
-        <Footer />
-      </main>
-    </main>
+        </>
   )
 }
 
