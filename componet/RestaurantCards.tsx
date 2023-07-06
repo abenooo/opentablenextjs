283be5d4e5da54c43620821fc3522b0afe5dc2a5
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-07-06 14:17:28 +0300

    Finished Ui

--- componet/RestaurantCards.tsx
+++ componet/RestaurantCards.tsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react"
+import Link from "next/link"
 function RestaurantCards() {
   return (
-    <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-    <Link href="/restaurant/banana">
-      <img
-        src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-        alt=""
-        className="w-full h-36"
-      />
-      <div className="p-1">
-        <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-        <div className="flex items-start">
-          <div className="flex mb-2">*****</div>
-          <p className="ml-2">77 reviews</p>
+    <Link href="/search">
+    <div className='flex flex-wrap py-3 mt-10 justify-star px-36'>
+      <div className='w-64 m-3 overflow-hidden border rounded cursor-pointer h-72'>
+        <img
+          src='https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg'
+          alt=''
+          className='w-full h-36'
+        />
+        <div className='p-1'>
+          <h3 className='mb-2 text-2xl font-bold'>Milestones Grill</h3>
+          <div className='flex items-start'>
+            <div className='flex mb-2'>*****</div>
+            <p className='ml-2'>77 reviews</p>
+          </div>
+          <div className='flex font-light capitalize text-reg'>
+            <p className='mr-3 '>Mexican</p>
+            <p className='mr-3'>$$$$</p>
+            <p>Toronto</p>
+          </div>
+          <p className='mt-1 text-sm font-bold'>Booked 3 times today</p>
         </div>
-        <div className="flex text-reg font-light capitalize">
-          <p className=" mr-3">Mexican</p>
-          <p className="mr-3">$$$$</p>
-          <p>Toronto</p>
-        </div>
-        <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
       </div>
+    </div>
     </Link>
-  </div>
   )
 }
 
