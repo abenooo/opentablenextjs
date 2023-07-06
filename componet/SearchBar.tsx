eee8153ac0d47fb026e61e1e7f6a01e395301ae6
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-07-06 10:36:40 +0300

    fix the error on search and button click

--- componet/SearchBar.tsx
+++ componet/SearchBar.tsx
@@ -4,31 +4,34 @@ import React, { useState } from 'react'
 
 function SearchBar() {
 
-    const router = useRouter();
-    const [location, setLocation] = useState("");
-    return (
+  const router = useRouter();
+  const [location, setLocation] = useState("");
+  return (
+    <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
+      <div className="flex justify-center py-3 m-auto text-lg text-left">
+        <input
+          className="rounded  mr-3 p-2 w-[450px]"
+          type="text"
+          value={location}
+          onChange={(e) => {
+            setLocation(e.target.value)
+          }}
+          placeholder="State, city or town"
+        />
+        <button className="py-2 text-white bg-red-600 rounded px-9"
+          onClick={() => {
+            if (location === "banana") return;
+            router.push("search")
+          }}>
+          Let's go
+        </button>
+      </div>
+    </div>
 
-        <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
-            <div className="text-left text-lg py-3 m-auto flex justify-center">
-                <input
-                    className="rounded  mr-3 p-2 w-[450px]"
-                    type="text"
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    placeholder="State, city or town"
-                />
-                <button className="rounded bg-red-600 px-9 py-2 text-white"
-                    onClick={() => {
-                        if (location === "banana") return;
-                        router.push("search")
-                    }}>
-                    Let's go
-                </button>
-            </div>
-        </div>
-
-    )
+  )
 }
 
 export default SearchBar
+
+
 
