563c86d7526d210043bc3e6849952385e546c93d
Infinity Dev1 <infinitydev1@InfinityVDI007.cbe.com.et>
2023-07-06 10:35:15 +0300

    Fix minor error

--- componet/Layout.tsx
+++ componet/Layout.tsx
@@ -3,15 +3,16 @@ import Footer from './Footer';
 import NavBar from './NavBar';
 
 const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
-  <main className="bg-gray-100 min-h-screen w-screen">
-  <main className="max-w-screen-2xl m-auto bg-white">
-
-        <NavBar />
-        {children}
-        <Footer />
-        </main>
-        </main>
-      
+  <>
+    <main className="bg-gray-100 min-h-screen w-screen">
+        <main className="max-w-screen-2xl m-auto bg-white">
+          <NavBar />
+    {children}
+    <Footer />
+    <main/>
+    </main>
+    </main>
+  </>
 );
 
 export default Layout;
